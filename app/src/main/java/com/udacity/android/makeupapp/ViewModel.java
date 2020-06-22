e7b504a69ff9b9a59e6dd6ab2d340e988d67b627
Katarzyna Skocka <katarzyna.skocka@vimn.com>
2020-06-22 17:11:46 +0200

    Add ViewModel

---
+++ app/src/main/java/com/udacity/android/makeupapp/ViewModel.java
@@ -0,0 +1,27 @@
+package com.udacity.android.makeupapp;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.udacity.android.makeupapp.api.model.Product;
+import com.udacity.android.makeupapp.database.ProductsDB;
+
+import java.util.List;
+
+import timber.log.Timber;
+
+public class ViewModel extends AndroidViewModel {
+
+    private LiveData<List<Product>> favorites;
+
+    public ViewModel(@NonNull Application application) {
+        super(application);
+        Timber.d("Getting favorite products from DB");
+        ProductsDB productsDB = ProductsDB.getInstance(this.getApplication());
+        favorites = productsDB.productDao().getFavorites();
+    }
+
+}
